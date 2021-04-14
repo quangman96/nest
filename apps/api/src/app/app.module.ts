5fc451d67bce9bac86469b0678f4df0a56066db8
manmeo96 <man.meo.69@gmail.com>
2021-04-14 15:19:51 +0700

    config to deploy

---
+++ apps/api/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common'
+
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+
+import { CoreModule } from '@backend/core'
+
+@Module({
+  imports: [CoreModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ libs/core/src/lib/core.resolver.ts
@@ -0,0 +1,14 @@
+import { Float, Query, Resolver } from "@nestjs/graphql";
+
+@Resolver()
+export class CoreResolver {
+  @Query(() => Float)
+  uptime() {
+    return process.uptime();
+  }
+
+  @Query(() => String)
+  hello() {
+    return 'Hello world!';
+  }
+}
